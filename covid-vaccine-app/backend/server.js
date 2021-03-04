1852cb4a1c1c2a4f04f21f34be5aa7273dfa0897
Ana Lucia Chin <ananime@gmail.com>
2021-03-04 15:08:03 -0500

    create profiles routes

--- covid-vaccine-app/backend/server.js
+++ covid-vaccine-app/backend/server.js
@@ -37,15 +37,15 @@ app.use(express.static("public"));
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
-// const usersRoutes = require("./routes/users");
-// const widgetsRoutes = require("./routes/widgets");
+
 const appointmentsRoutes = require("./routes/appointments");
+const profilesRoutes = require("./routes/profiles");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-// app.use("/api/users", usersRoutes(db));
-// app.use("/api/widgets", widgetsRoutes(db));
+
 app.use("/api/appointments", appointmentsRoutes(db));
+app.use("/api/profiles", profilesRoutes(db));
 
 // Note: mount other resources here, using the same pattern above
 
