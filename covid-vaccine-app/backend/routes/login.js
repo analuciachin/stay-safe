35e8946054bbc5efdc7066f1a8b9ef10bf4c3882
analuciachin <ananime@gmail.com>
2021-03-06 02:51:35 +0000

    work on the login route

--- covid-vaccine-app/backend/routes/login.js
+++ covid-vaccine-app/backend/routes/login.js
@@ -11,17 +11,36 @@ module.exports = (db) => {
     //   res.status(401).send("Email or password is empty");
     //   return;
     // }
-
-    if (nurse === null) {
+    console.log("nurse_username ", nurse_username);
+    console.log("patient_email ", patient_email);
+    if (!nurse_username && patient_email) {
       return db
-        .query(`SELECT * FROM parents WHERE email = $1;`, [patient_email])
+        .query(`SELECT * FROM patients WHERE email = $1;`, [patient_email])
         .then((data) => {
           console.log("data ", data);
-          // const patient_password = data.rows[0].patient_password;
-          // if (parent_password == password) {
-          //   const parent = data.rows;
-          //   res.json({ parent });
-          // }
+          const patient_password = data.rows[0].password;
+          if (patient_password == password) {
+            const patient = data.rows;
+            res.json({ patient });
+          } else {
+            res.status(401).send("Invalid password");
+          }
+        })
+        .catch((err) => {
+          res.status(500).json({ error: err.message });
+        });
+    } else if (nurse_username && !patient_email) {
+      return db
+        .query(`SELECT * FROM nurses WHERE username = $1;`, [nurse_username])
+        .then((data) => {
+          console.log("data ", data);
+          const nurse_password = data.rows[0].password;
+          if (nurse_password == password) {
+            const nurse = data.rows;
+            res.json({ nurse });
+          } else {
+            res.status(401).send("Invalid password");
+          }
         })
         .catch((err) => {
           res.status(500).json({ error: err.message });
