9d5a7cb60958e33f7e56952aaaeec98228e6747b
analuciachin <ananime@gmail.com>
2021-03-06 02:59:49 +0000

    add conditions for invalid username, email and password

--- covid-vaccine-app/backend/routes/login.js
+++ covid-vaccine-app/backend/routes/login.js
@@ -7,10 +7,6 @@ module.exports = (db) => {
     const patient_email = req.body.email || null;
     const password = req.body.password;
 
-    // if (email === "" || password === "") {
-    //   res.status(401).send("Email or password is empty");
-    //   return;
-    // }
     console.log("nurse_username ", nurse_username);
     console.log("patient_email ", patient_email);
     if (!nurse_username && patient_email) {
@@ -18,12 +14,16 @@ module.exports = (db) => {
         .query(`SELECT * FROM patients WHERE email = $1;`, [patient_email])
         .then((data) => {
           console.log("data ", data);
-          const patient_password = data.rows[0].password;
-          if (patient_password == password) {
-            const patient = data.rows;
-            res.json({ patient });
+          if (data.rows.length === 0) {
+            res.status(401).send("Invalid email");
           } else {
-            res.status(401).send("Invalid password");
+            const patient_password = data.rows[0].password;
+            if (patient_password == password) {
+              const patient = data.rows;
+              res.json({ patient });
+            } else {
+              res.status(401).send("Invalid password");
+            }
           }
         })
         .catch((err) => {
@@ -34,12 +34,16 @@ module.exports = (db) => {
         .query(`SELECT * FROM nurses WHERE username = $1;`, [nurse_username])
         .then((data) => {
           console.log("data ", data);
-          const nurse_password = data.rows[0].password;
-          if (nurse_password == password) {
-            const nurse = data.rows;
-            res.json({ nurse });
+          if (data.rows.length === 0) {
+            res.status(401).send("Invalid username");
           } else {
-            res.status(401).send("Invalid password");
+            const nurse_password = data.rows[0].password;
+            if (nurse_password == password) {
+              const nurse = data.rows;
+              res.json({ nurse });
+            } else {
+              res.status(401).send("Invalid password");
+            }
           }
         })
         .catch((err) => {
