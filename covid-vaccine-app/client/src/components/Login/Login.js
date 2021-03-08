46364bacabf8609849a11f128e33841b19c7a9cf
analuciachin <ananime@gmail.com>
2021-03-08 16:10:23 +0000

    add conditional rendering of the login form

--- covid-vaccine-app/client/src/components/Login/Login.js
+++ covid-vaccine-app/client/src/components/Login/Login.js
@@ -1,13 +1,30 @@
 import { useState } from "react";
 import axios from "axios";
 
-export default function Login() {
+export default function Login({ userSelected }) {
   return (
-    <form>
-      <label htmlFor="name">Email</label>
-      <input type="text" name="email" />
-      <label htmlFor="password">Password</label>
-      <input type="password" name="password" />
-    </form>
+    <>
+      {console.log("userSelected ", userSelected)}
+      {userSelected === null ? (
+        <h3>Please select a type of user to login</h3>
+      ) : (
+        <form>
+          {userSelected === "Patient" ? (
+            <div>
+              <label htmlFor="name">Email</label>
+              <input type="text" name="email" />
+            </div>
+          ) : (
+            <div>
+              <label htmlFor="name">Username</label>
+              <input type="text" name="email" />
+            </div>
+          )}
+
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" />
+        </form>
+      )}
+    </>
   );
 }
