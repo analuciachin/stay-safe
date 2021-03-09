d1a9528c88954f839e71be53382c9a46631fca0c
analuciachin <ananime@gmail.com>
2021-03-09 15:43:45 +0000

    update patients routes

--- covid-vaccine-app/backend/routes/patients.js
+++ covid-vaccine-app/backend/routes/patients.js
@@ -116,16 +116,98 @@ module.exports = (db) => {
 
   router.get("/:id/appointments", (req, res) => {
     db.query(
-      `SELECT appointments.* 
-      FROM appointments 
-      LEFT JOIN patients ON appointments.patient_id = patients.id
-      LEFT JOIN nurses ON appointments.nurse_id = nurses.id
+      `SELECT appts.id, 
+      appts.appt_date, 
+      appts.is_high_priority, 
+      nurses.first_name as nurse_first_name, 
+      nurses.last_name as nurse_last_name
+      FROM appointments as appts
+      LEFT JOIN patients ON appts.patient_id = patients.id
+      LEFT JOIN nurses ON appts.nurse_id = nurses.id
       WHERE patients.id = $1;`,
       [req.params.id]
     )
       .then((data) => {
         const appointments = data.rows;
         res.json({ appointments });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  router.post("/:id/appointments", (req, res) => {
+    const appt_date = req.body.appt_date;
+    const nurse_id = req.body.nurse_id;
+    const patient_id = req.params.id;
+    const is_high_priority = req.body.is_high_priority;
+
+    if (appt_date === "" || nurse_id === "" || patient_id === "") {
+      res.status(401).send("There are empty fields in the form.");
+      return;
+    }
+
+    return db
+      .query(
+        `
+        INSERT INTO appointments (appt_date, nurse_id, patient_id, is_high_priority)
+        VALUES ($1, $2, $3, $4)
+        RETURNING *;
+      `,
+        [appt_date, nurse_id, patient_id, is_high_priority]
+      )
+      .then((data) => {
+        const appointment = data.rows;
+        res.json({ appointment });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  router.put("/:patientId/appointments/:appointmentId", (req, res) => {
+    const appt_id = req.params.appointmentId;
+    const patient_id = req.params.patientId;
+    const appt_date = req.body.appt_date;
+    const nurse_id = req.body.nurse_id;
+    const is_high_priority = req.body.is_high_priority;
+
+    if (appt_date === "" || nurse_id === "" || patient_id === "") {
+      res.status(401).send("There are empty fields in the form.");
+      return;
+    }
+
+    return db
+      .query(
+        `
+        UPDATE appointments 
+        SET 
+          appt_date = $1,
+          nurse_id = $2,
+          patient_id = $3,
+          is_high_priority = $4
+        WHERE id = $5
+        RETURNING *;
+      `,
+        [appt_date, nurse_id, patient_id, is_high_priority, appt_id]
+      )
+      .then((data) => {
+        const appointment = data.rows;
+        res.json({ appointment });
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  router.delete("/:patientId/appointments/:appointmentId", (req, res) => {
+    db.query(`DELETE FROM appointments WHERE id = $1;`, [
+      req.params.appointmentId,
+    ])
+      .then((data) => {
+        res.status(200).json({
+          message: "Appointment deleted successfuly!",
+        });
       })
       .catch((err) => {
         res.status(500).json({ error: err.message });
