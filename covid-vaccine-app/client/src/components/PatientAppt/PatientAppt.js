c9f482781016e29b93eff7fe0d4af5375386ff62
analuciachin <ananime@gmail.com>
2021-03-10 19:52:18 +0000

    render appt info

--- covid-vaccine-app/client/src/components/PatientAppt/PatientAppt.js
+++ covid-vaccine-app/client/src/components/PatientAppt/PatientAppt.js
@@ -19,7 +19,26 @@ export default function PatientAppt({ user }) {
 
   return (
     <>
-      <p>PatientAppt component</p>
+      {appointment && appointment.length > 0 ? (
+        <div>
+          <p>
+            Appointment Date: <span>{appointment[0].appt_date}</span>
+          </p>
+
+          <p>
+            Nurse:{" "}
+            <span>
+              {appointment[0].nurse_first_name} {appointment[0].nurse_last_name}
+            </span>
+          </p>
+          <p>
+            Is this a high priority appointment?{" "}
+            {appointment[0].is_high_priority ? "Yes" : "No"}{" "}
+          </p>
+        </div>
+      ) : (
+        <h3>Please book an appointment to get your COVID-19 shot.</h3>
+      )}
     </>
   );
 }
