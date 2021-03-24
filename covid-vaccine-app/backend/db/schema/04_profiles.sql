1603950a7b656dc0a64ca00d46bd2848af9e589a
analuciachin <ananime@gmail.com>
2021-03-24 20:52:54 +0000

    implement the create patient profile functionality

--- covid-vaccine-app/backend/db/schema/04_profiles.sql
+++ covid-vaccine-app/backend/db/schema/04_profiles.sql
@@ -5,7 +5,7 @@ CREATE TABLE profiles (
   patient_id INTEGER REFERENCES patients(id) ON DELETE CASCADE,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
-  age INTEGER,
+  age VARCHAR(255) NOT NULL,
   has_chronic_conditions BOOLEAN DEFAULT false,
   is_health_care_worker BOOLEAN DEFAULT false,
   is_staff_senior_care BOOLEAN DEFAULT false
