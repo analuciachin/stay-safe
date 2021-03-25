6f23fdb947f134952c7cec678e54df10df9da7a3
analuciachin <ananime@gmail.com>
2021-03-25 17:28:25 +0000

    add create profile form validation

--- covid-vaccine-app/client/src/components/PatientProfile/PatientProfile.js
+++ covid-vaccine-app/client/src/components/PatientProfile/PatientProfile.js
@@ -13,12 +13,15 @@ export default function PatientProfile({ user }) {
   });
 
   const [patientProfile, setPatientProfile] = useState(null);
+  const [isFormFilled, setIsFormFilled] = useState(null);
 
   let history = useHistory();
 
   const submitHandler = (event) => {
     event.preventDefault();
-    createProfile(profileInfo);
+    if (formValidation()) {
+      createProfile(profileInfo);
+    } else return false;
   };
 
   const createProfile = (profileInfo) => {
@@ -36,27 +39,57 @@ export default function PatientProfile({ user }) {
         console.log(response);
         const new_profile = response.data.profile;
         setPatientProfile(new_profile);
-
+        setIsFormFilled(null);
         history.push(`/patients/${user[0].id}`);
       })
       .catch(function (error) {
+        setIsFormFilled(null);
         console.log(error);
       });
   };
 
-  useEffect(() => console.log(patientProfile), [patientProfile]);
+  const formValidation = () => {
+    if (
+      profileInfo.first_name &&
+      profileInfo.last_name &&
+      profileInfo.age > 0 &&
+      profileInfo.age < 5 &&
+      (profileInfo.has_chronic_conditions === "0" ||
+        profileInfo.has_chronic_conditions === "1") &&
+      (profileInfo.is_health_care_worker === "0" ||
+        profileInfo.is_health_care_worker === "1") &&
+      (profileInfo.is_staff_senior_care === "0" ||
+        profileInfo.is_staff_senior_care === "1")
+    ) {
+      setIsFormFilled(true);
+      return true;
+    } else {
+      setIsFormFilled(false);
+      return false;
+    }
+  };
+
+  const clearError = () => {
+    setIsFormFilled(null);
+  };
+
+  useEffect(() => console.log(isFormFilled), [isFormFilled]);
 
   return (
     <>
+      {isFormFilled === false && (
+        <h2>Please fill out all the field in the form.</h2>
+      )}
       <h1>Patient Profile Information</h1>
       <form onSubmit={submitHandler}>
         <label htmlFor="first-name">First Name</label>
         <input
           type="text"
           name="first-name"
-          onChange={(event) =>
-            setProfileInfo({ ...profileInfo, first_name: event.target.value })
-          }
+          onChange={(event) => {
+            setProfileInfo({ ...profileInfo, first_name: event.target.value });
+            clearError();
+          }}
           value={profileInfo.first_name}
         />
 
@@ -64,20 +97,21 @@ export default function PatientProfile({ user }) {
         <input
           type="text"
           name="last-name"
-          onChange={(event) =>
-            setProfileInfo({ ...profileInfo, last_name: event.target.value })
-          }
+          onChange={(event) => {
+            setProfileInfo({ ...profileInfo, last_name: event.target.value });
+            clearError();
+          }}
           value={profileInfo.last_name}
         />
 
         <label htmlFor="age">Select your age in the range below.</label>
         <select
           name="age"
-          id="age"
           value={profileInfo.age}
-          onChange={(event) =>
-            setProfileInfo({ ...profileInfo, age: event.target.value })
-          }
+          onChange={(event) => {
+            setProfileInfo({ ...profileInfo, age: event.target.value });
+            clearError();
+          }}
         >
           <option value="placeholder">Select an option</option>
           <option value="1">over 80 years old</option>
@@ -86,53 +120,58 @@ export default function PatientProfile({ user }) {
           <option value="4">under 45 years old</option>
         </select>
 
-        <label htmlFor="age">Do you have any chronic conditions?</label>
+        <label htmlFor="chronic-conditions">
+          Do you have any chronic conditions?
+        </label>
         <select
           name="has_chronic_conditions"
-          id="has_chronic_conditions"
           value={profileInfo.has_chronic_conditions}
-          onChange={(event) =>
+          onChange={(event) => {
             setProfileInfo({
               ...profileInfo,
               has_chronic_conditions: event.target.value,
-            })
-          }
+            });
+            clearError();
+          }}
         >
           <option value="placeholder">Select an option</option>
           <option value="true">Yes</option>
           <option value="false">No</option>
         </select>
 
-        <label htmlFor="age">Are you a health worker professional?</label>
+        <label htmlFor="health-worker">
+          Are you a health worker professional?
+        </label>
         <select
           name="is_health_care_worker"
-          id="is_health_care_worker"
           value={profileInfo.is_health_care_worker}
-          onChange={(event) =>
+          onChange={(event) => {
             setProfileInfo({
               ...profileInfo,
               is_health_care_worker: event.target.value,
-            })
-          }
+            });
+            clearError();
+          }}
         >
           <option value="placeholder">Select an option</option>
           <option value="true">Yes</option>
           <option value="false">No</option>
         </select>
 
-        <label htmlFor="age">
+        <label htmlFor="staff-senior-care">
           Are you a staff member in a senior care home?
         </label>
         <select
           name="is_staff_senior_care"
           id="is_staff_senior_care"
           value={profileInfo.is_staff_senior_care}
-          onChange={(event) =>
+          onChange={(event) => {
             setProfileInfo({
               ...profileInfo,
               is_staff_senior_care: event.target.value,
-            })
-          }
+            });
+            clearError();
+          }}
         >
           <option value="placeholder">Select an option</option>
           <option value="true">Yes</option>
