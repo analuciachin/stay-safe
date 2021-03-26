b0b46b8d2aaf5a8981c8c3a6e52c0a6d4140bd66
analuciachin <ananime@gmail.com>
2021-03-26 18:47:24 +0000

    add styles to Login and UserOptions components

--- covid-vaccine-app/client/src/components/UserOptions/UserOptions.js
+++ covid-vaccine-app/client/src/components/UserOptions/UserOptions.js
@@ -1,6 +1,12 @@
 import { useState } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "./UserOptions.css";
 
 import Login from "../Login/Login";
+import vaccine from "../../images/health-doctor-vaccine.svg";
 
 export default function UserOptions({ getUserLogged }) {
   const users = ["Nurse", "Patient"];
@@ -13,14 +19,28 @@ export default function UserOptions({ getUserLogged }) {
 
   return (
     <>
-      <ul>
-        {users.map((user) => (
-          <li key={user}>
-            <button onClick={() => getSelectedUser(user)}>{user}</button>
-          </li>
-        ))}
-      </ul>
-      <Login userSelected={userSelected} getUserLogged={getUserLogged} />
+      <Row className="align-items-center">
+        <Col>
+          <img src={vaccine} alt="doctor-vaccine" className="vaccine-img" />
+          <p className="text-center title mt-5">Stay Safe</p>
+        </Col>
+        <Col>
+          <h1 className="text-center mt-3 mb-5">Login</h1>
+          <ul className="text-center mt-3 mb-5">
+            {users.map((user) => (
+              <li key={user}>
+                <button
+                  onClick={() => getSelectedUser(user)}
+                  className="user-btn"
+                >
+                  {user}
+                </button>
+              </li>
+            ))}
+          </ul>
+          <Login userSelected={userSelected} getUserLogged={getUserLogged} />
+        </Col>
+      </Row>
     </>
   );
 }
