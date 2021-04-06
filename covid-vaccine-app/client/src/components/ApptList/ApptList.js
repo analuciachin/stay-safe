7c06be9de3aa1c4b4327160b6d84b2008ee12d68
analuciachin <ananime@gmail.com>
2021-04-06 20:07:18 +0000

    sort appts by date/time

--- covid-vaccine-app/client/src/components/ApptList/ApptList.js
+++ covid-vaccine-app/client/src/components/ApptList/ApptList.js
@@ -5,12 +5,30 @@ import { useState, useEffect } from "react";
 export default function ApptList({ user }) {
   const [appointments, setAppointments] = useState(null);
 
-  useEffect(() => {
-    axios.get(`/api/nurses/${user.id}/appointments`).then((response) => {
-      console.log(response.data);
-      const appts = response.data;
-      setAppointments(appts);
+  const sortByDateTime = (appts) => {
+    for (let appt of appts) {
+      appt.new_datetime = appt.appt_date.substring(0, 19);
+      appt.new_date = new Date(appt.new_datetime);
+    }
+    appts.sort(function (a, b) {
+      console.log("sort");
+      return a.new_date - b.new_date;
     });
+    console.log(appts);
+  };
+
+  useEffect(() => {
+    axios
+      .get(`/api/nurses/${user.id}/appointments`)
+      .then((response) => {
+        console.log(response.data.appointments);
+        const appts = response.data.appointments;
+        setAppointments(appts);
+      })
+      .then(() => {
+        console.log("here");
+        sortByDateTime(appointments);
+      });
   }, []);
 
   return (
